e23f2085b70a7d9bc26eadce2fcc0b87ee15e353
Ashish Bhosle <140106733+AshishBhosle17@users.noreply.github.com>
2024-05-07 20:48:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
-import { BrowserRouter as Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Navbaar from './components/Navbaar';
 import Home from './components/Home';
 import Register from './components/Register';
@@ -11,10 +11,12 @@ function App() {
     <>
       
         <Navbaar />
+        <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
         </Routes>
+        <BrowserRouter />
       </>
   );
 }
